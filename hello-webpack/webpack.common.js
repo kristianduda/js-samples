681ea2baffbc22681d1cd6bd3ced96285333dc4f
kristianduda <kristianduda83@gmail.com>
2019-02-08 04:00:10 +0100

    add sass support

--- hello-webpack/webpack.common.js
+++ hello-webpack/webpack.common.js
@@ -25,6 +25,14 @@ module.exports = {
           'css-loader'
         ]
       },
+      {
+        test: /\.scss$/,
+        use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader"
+        ]
+      },
       { 
         test: /\.js$/, 
         exclude: /node_modules/, 
